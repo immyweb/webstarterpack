0181521f2b40f5faaac12a7355b404c547a82ed6
Imran Oozeerally <imranoozeerally@Imrans-MacBook-Pro.local>
2016-11-02 19:10:49 +0000

    Removed unused code

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,9 @@
 import $ from 'jquery';
 import globalHeader from './components/global-header/index';
-import panelHero from './components/panel-hero/index';
-import panelVideo from './components/panel-video/index';
-import panelMessage from './components/panel-message/index';
 
 // run scripts on DOM ready
 $(document).ready(() => {
 
     globalHeader.init();
-    panelHero.init();
-    panelVideo.init();
-    panelMessage.init();
 
 });
